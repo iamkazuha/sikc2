d639d3aaba845072d72cc2e0c0e8586537d2b5e9
trisna <trisnayg02@gmail.com>
2024-12-17 19:34:54 +0700

    Second Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
-export default {
+module.exports = {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
     build: {
-        outDir: 'public/build', // Tentukan output folder
+        outDir: "public/build", // Good practice to specify output directory
+        emptyOutDir: true, // Recommended: clears output directory before building
+    },
+    resolve: {
+        alias: {
+            "@": "/resources/js", // Optional: convenient import alias
+        },
     },
 });
-
